400b50f57f3cbe36dd52899b3b3d510ac5b92ade
sankar <sankaranarayananm@sahaj.ai>
2023-02-22 23:57:31 +0530

    Add details to the table dynamically

--- OrderHistory.js
+++ OrderHistory.js
@@ -14,7 +14,37 @@ function getOrderHistory() {
     .then((response) => {
       if (response.error) {
         document.getElementById("response").innerHTML = response.error;
-      } 
+      } else {
+        if (response.length == 0) {
+          document.getElementsByClassName("insert-record").innerHtml =
+            "No Transactions found";
+          return;
+        } else {
+          let table = document.getElementById("tableData");
+          for (
+            var transaction = 0;
+            transaction < response.length;
+            transaction++
+          ) {
+            let row = table.insertRow(-1);
+
+            let c1 = row.insertCell(0);
+            let c2 = row.insertCell(1);
+            let c3 = row.insertCell(2);
+            let c4 = row.insertCell(3);
+            let c5 = row.insertCell(4);
+
+            // Add data to c1 and c2
+            c1.innerText = response[transaction].orderId;
+            c2.innerText = response[transaction].type;
+            c3.innerText = response[transaction].quantity;
+            c4.innerText = response[transaction].price;
+            c5.innerText = response[transaction].esopType;
+
+        
+          }
+        }
+      }
     });
 }
 
